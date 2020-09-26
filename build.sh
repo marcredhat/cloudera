33216e5d25188ff8d8c2ee5a0aa47d2939d71a8c
Marc Chisinevski <mchisine@redhat.com>
2020-09-26 14:14:07 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cdpcontainermarc=$(buildah from registry.centos.org/centos/centos:latest)
 #export CONTAINER_FS=$(buildah mount)
 buildah run $cdpcontainermarc yum update -y && \
-buildah run $cdpcontainermarc yum  install git -y && \
+buildah run $cdpcontainermarc yum  install git udisks2 -y && \
 buildah run $cdpcontainermarc git clone https://github.com/carrossoni/CDPDCTrial/
 
 buildah config --cmd /bin/bash ${cdpcontainermarc} && \
