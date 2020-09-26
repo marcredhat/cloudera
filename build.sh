a90f09dae3be3d40ec1c0b2be5eac812927332ca
Marc Chisinevski <mchisine@redhat.com>
2020-09-26 14:06:48 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+cdpcontainermarc=$(buildah from registry.centos.org/centos/centos:latest)
+#export CONTAINER_FS=$(buildah mount)
+buildah run $cdpcontainermarc yum update -y && \
+buildah run $cdpcontainermarc yum  install git -y && \
+buildah run $cdpcontainermarc git clone https://github.com/carrossoni/CDPDCTrial/
+
+buildah config --cmd /bin/bash ${cdpcontainermarc} && \
+
+buildah run $cdpcontainermarc -- sh -c "git clone https://github.com/carrossoni/CDPDCTrial/" && \
+
+buildah run $cdpcontainermarc -- sh -c "chmod +x ./CDPDCTrial/centosvmCDP.sh" && \
+
+buildah run $cdpcontainermarc -- sh -c "./CDPDCTrial/centosvmCDP.sh"
